95960f0d7aef66c4fdbf3df8eada4dd91b492243
lars-m <lam@cphbusiness.dk>
2019-01-02 10:42:06 +0100

    minor

--- gatsby-config.js
+++ gatsby-config.js
@@ -50,7 +50,7 @@ module.exports = {
         // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
         // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
         display: "standalone",
-        icon: "src/images/logo2.png" // This path is relative to the root of the site.
+        icon: "src/images/logo3.png" // This path is relative to the root of the site.
       }
     },
     "gatsby-plugin-offline"
