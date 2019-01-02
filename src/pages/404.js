00e82cb4172856f21c13636ae475a29d37cba950
lars-m <lam@cphbusiness.dk>
2019-01-02 10:51:57 +0100

    minor

--- src/pages/404.js
+++ src/pages/404.js
@@ -4,7 +4,7 @@ import Layout from "../components/layout";
 export default () => (
   <Layout>
     <h1>Page Not Found</h1>
-    <h3 style={{marginTop:75,color:"darkred"}}>
+    <h3 style={{marginTop:70,color:"darkred"}}>
       The requested resource was not found!
     </h3>
   </Layout>
