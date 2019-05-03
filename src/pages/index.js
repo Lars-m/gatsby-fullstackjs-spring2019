072d9bc6c3b35b2a7e02a9cbfd5fd34eecd9635e
lars-m <lam@cphbusiness.dk>
2019-05-03 11:20:07 +0200

    fixes

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,10 @@
 import React from "react";
 import Layout from "../components/layout";
-import { navigate } from "gatsby"
+import { navigate } from "gatsby";
 export default () => {
-  return <div></div>
-}
- 
-
+  return (
+    <Layout>
+      <div />
+    </Layout>
+  );
+};
